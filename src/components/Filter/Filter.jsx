a51c44318c91c84ec9661cc21303ae08a2ddaac1
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-15 15:12:06 +0200

    Update Filter.jsx
    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,7 +6,8 @@ export const Filter = ({ filter, changeFilterInput }) => (
   <label>
     <input
       type="text"
-      name={filter}
+      name="filter"
+      value={filter}
       onChange={changeFilterInput}
       placeholder="Find contacts by name"
     />
