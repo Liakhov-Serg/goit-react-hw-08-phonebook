da3638862b72b7d47e1c8759dcd12562811fc81c
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-18 10:28:02 +0200

    Update App.jsx
    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,27 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import './App.module.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const formSubmit = (name, number) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    const enterContacts = contacts.some(
-      i =>
-        (i.name === contact.name.toLowerCase() &&
-          i.number === contact.number) ||
-        i.number === contact.number
-    );
-    enterContacts
-      ? alert(`${name} or ${number} is already in contacts`)
-      : setContacts([contact, ...contacts]);
-  };
-
-  const changeFilterInput = e => {
-    setFilter(e.target.value);
-  };
-
-  const findContacts = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  const deleteContact = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-    setFilter('');
-  };
-
-  return (
+   return (
     <section>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={formSubmit} />
+      <ContactForm />
+
       <h2>Contacts</h2>
-      <Filter filter={filter} changeFilterInput={changeFilterInput} />
-      <ContactList contacts={findContacts()} deleteContact={deleteContact} />
+      <Filter />
+      <ContactList />
     </section>
   );
-};
+
+  
+  
+   
+  };
+
+  
+
+  
