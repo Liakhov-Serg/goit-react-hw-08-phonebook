7e6a6c519a459ec63ad4c9324cdbed4944c41811
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-20 21:31:36 +0200

    Update Filter.jsx
    update Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -8,17 +8,13 @@ export const Filter = () => {
   const dispatch = useDispatch();
   const filter = useSelector(getFilter);
 
-  const onChangeFilter = e => {
-    dispatch(setFilter(e.target.value));
-  };
-
-  return (
+    return (
     <label>
       <input
         type="text"
         name="filter"
         value={filter}
-        onChange={onChangeFilter}
+        onChange={e => dispatch(setFilter(e.target.value))}
         placeholder="Find contacts by name"
       />
     </label>
