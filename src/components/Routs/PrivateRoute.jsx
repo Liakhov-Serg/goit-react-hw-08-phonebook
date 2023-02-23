649d4a9d09250e056e53b18f18b2a7355cd80223
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-23 15:27:03 +0200

    111
    111

--- src/components/Routs/PrivateRoute.jsx
+++ src/components/Routs/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
-import { } from 'react-router-dom';
-// import authSelectors from '../../redux/auth/auth-selectors';
+import {Navigate } from 'react-router-dom';
+import authSelectors from '../../redux/auth/auth-selectors';
 
-const PrivateRoute = ({ }) => {
+const PrivateRoute = ({ children}) => {
     const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
     return isLoggedIn ? children : <Navigate to="/" replace />;
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import 'react-toastify/dist/ReactToastify.css';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor } from './redux/store';
 import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-
+import { App } from 'components/App';
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-       
-      <App />
+     <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
